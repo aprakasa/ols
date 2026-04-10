0865045c3391dbf1ce827c14d56400e9093600df
aprakasa <prakasa.arya@gmail.com>
2026-04-11 06:41:19 +0700

    fix: handle SIGTERM properly in entrypoint to prevent orphaned processes
    Replace exec with explicit signal trapping so both the watcher and OLS
    main process are cleanly terminated on container stop.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-set -e
+
+MAIN_PID=""
+WATCHER_PID=""
+
+_term() {
+    kill "$WATCHER_PID" "$MAIN_PID" 2>/dev/null || true
+    wait
+    exit 0
+}
+trap _term TERM INT HUP
 
 /watch-htaccess.sh &
+WATCHER_PID=$!
 
-exec "$@"
+"$@" &
+MAIN_PID=$!
+
+wait "$MAIN_PID"
